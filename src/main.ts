d9bea937fd6afb9c5bc4e01251d5d5720ce05ce5
Dhafer Alzubairi <dx.dhafer@gmail.com>
2023-09-23 17:38:44 +0200

    rewriting into full express app

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,51 @@
-import { ApolloServer } from 'apollo-server';
+// import { ApolloServer } from 'apollo-server';
 
+// import typeDefs from './schema';
+// import resolvers from './resolvers';
+
+// const server = new ApolloServer({
+// 	typeDefs: typeDefs,
+// 	resolvers: resolvers,
+// 	cors: {
+// 		origin: [
+// 			'https://e-commerce-deployment.netlify.app',
+// 			'http://localhost:3000',
+// 		],
+// 		// origin: '*',
+// 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// 		allowedHeaders: ['Content-Type', 'Authorization'],
+// 	},
+// });
+
+// server.listen().then(({ url }) => {
+// 	console.log(`🚀  Server ready at ${url}`);
+// });
+
+import express from 'express';
+import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+const app = express();
+
+// Create an instance of the Apollo server.
 const server = new ApolloServer({
-	typeDefs: typeDefs,
-	resolvers: resolvers,
+	typeDefs,
+	resolvers,
 	cors: {
-		// origin: [
-		// 	'https://e-commerce-deployment.netlify.app',
-		// 	'http://localhost:3000',
-		// ],
-		origin: '*',
+		origin: [
+			'https://e-commerce-deployment.netlify.app',
+			'http://localhost:3000',
+		],
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	},
 });
 
-server.listen().then(({ url }) => {
-	console.log(`🚀  Server ready at ${url}`);
+// Apply the Apollo server middleware to the Express application.
+app.use(server.getMiddleware());
+
+// Start the Express server.
+app.listen(3000, () => {
+	console.log('Server is listening on port 3000');
 });
